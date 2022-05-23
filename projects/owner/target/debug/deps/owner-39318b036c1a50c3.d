3fa2c53192ccaf4c077651916e30db637aca6fd4
11lization <11lization@naver.com>
2022-05-23 10:54:14 +0000

    owner

---
+++ projects/owner/target/debug/deps/owner-39318b036c1a50c3.d
@@ -0,0 +1,5 @@
+/home/ubuntu/rust/projects/owner/target/debug/deps/owner-39318b036c1a50c3: src/main.rs
+
+/home/ubuntu/rust/projects/owner/target/debug/deps/owner-39318b036c1a50c3.d: src/main.rs
+
+src/main.rs:
---
+++ projects/owner/target/debug/owner.d
@@ -0,0 +1,1 @@
+/home/ubuntu/rust/projects/owner/target/debug/owner: /home/ubuntu/rust/projects/owner/src/main.rs
